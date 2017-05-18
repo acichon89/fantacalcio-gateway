7cc85efc19d15b8d0f46ea4bb97eb854087c3846
acichon <acichon89@gmail.com>
2017-05-18 23:23:31 +0200

    initial commit

---
+++ src/main/java/com/javangarda/fantacalcio/gateway/FantacalcioGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.javangarda.fantacalcio.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantacalcioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantacalcioGatewayApplication.class, args);
+	}
+}
